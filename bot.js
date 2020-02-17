2cb90e02917362fb1d9263dd8c79185e82e220a8
wyattscarpenter <wyattscarpenter@gmail.com>
2020-02-17 13:00:12 -0600

    i think this has to be a function, not a promise

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-       setInterval(message.reply(think()), 1000);
+       setInterval(() => {message.reply(think())}, 1000);
     }
 });
 
