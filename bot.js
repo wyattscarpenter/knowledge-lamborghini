254b57ce6fe0d865902c857ea3d9c2c5e82aa0ff
wyattscarpenter <wyattscarpenter@gmail.com>
2020-02-17 12:58:10 -0600

    update bot.js to make an example to test

--- bot.js
+++ bot.js
@@ -1,15 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var example = ["a", "b", "c"];
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-       message.reply('pong');
+       setInterval(message.reply(think()), 1000);
     }
 });
 
+function think(){
+    var s = example.shift();
+    if(s){
+        return s;
+    } else {
+        clearInterval();
+    }
+}
+        
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
