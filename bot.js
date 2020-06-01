6be785e6721d7b96d72096a31e814072df182570
wyattscarpenter <wyattscarpenter@gmail.com>
2020-06-01 07:27:45 -0500

    allow things like which pokemon, what pokemon

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('message', message => {
     }
 
     //extremely dumb features
-    if (/.*who.*pok.?mon.*/.test(message.content.toLowerCase())) {
+    if (/.*wh.*pok.?mon.*/.test(message.content.toLowerCase())) {
         channel.send("It's https://en.wikipedia.org/wiki/Special:Random");
     }
     if (message.content.toLowerCase().startsWith("eval")) {
