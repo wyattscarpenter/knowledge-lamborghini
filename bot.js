884effa4224f117878d5b50b95cafc02e85fe82f
wyattscarpenter <wyattscarpenter@gmail.com>
2020-08-14 10:08:16 -0500

    will this set the activity

--- bot.js
+++ bot.js
@@ -9,8 +9,7 @@ var responses = {"hewwo": "perish"};
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ activity: { name: 'with discord.js' }, status: 'idle' })
-    //client.user.setGame('Nicomachean Ethics');
+    //client.user.setActivity('Nicomachean Ethics');
     //client.user.setStatus("https://discord.com/api/oauth2/authorize?client_id=678989133196165152&permissions=0&scope=bot");
 });
 
