06981aa6400f56063895fa046d1bd8136239b1d6
wyattscarpenter <wyattscarpenter@gmail.com>
2020-08-18 05:33:14 -0500

    excise dice functionality to put it in its own package
    this will probably end up in https://www.npmjs.com/package/nicedice and/or https://github.com/wyattscarpenter/nicedice

--- bot.js
+++ bot.js
@@ -24,25 +24,14 @@ client.on('message', message => {
   if (message.content.toLowerCase() === 'stop!') {
     stop();
   }
-
-  //dice features.
-  /* tentative dice grammar:
-  //note that we observe pemdas, and do NOT care about whitespace
-  expression -> arithmetic | roll | subexpression
-  roll -> subexpression die-symbol subexpression
-  arithmetic -> arithmetic * arithmetic |  arithmetic / arithmetic | subexpression | addition
-  addition -> addition + addition |  addition - addition | subexpression | number
-  subexpression -> ( expression ) | number | advantage roll | disadvantage roll | min roll | max roll
-  die-symbol -> ! | d
-  number -> anything the implementation language will take as a number I guess
-  */
+/*
   if(message.content.toLowerCase().includes('nice dice')){
     message.channel.send("Sponsored by NiceDice™");
   }
   if((result = roll(message.content.toLowerCase())).valid){
       message.channel.send(result.result);
   }
-
+*/
   //extremely dumb features
   if (/.*wh.*po.?k.?t?\s?mon.*/.test(message.content.toLowerCase())) { //who's that pokemon
     https.request('http://commons.wikimedia.org/w/api.php?action=query&generator=random&grnnamespace=6',//image
@@ -85,65 +74,6 @@ function think(){
 function stop(){
   clearInterval(interval);
   interval = 0;
-}
-
-function roll(string){
-  var valid = true;
-  var result = "";
-  var total = 0;
-  
-  var symbolindex = 0;
-  function pop(){
-    return string[symbolindex++];
-  }
-  function peek(){
-    return string[symbolindex];
-  }
-  
-  function expression(){ //this will have to be expanded later
-    var lhs = number() || 1; //allow this to be empty so the user can say eg "d6"
-    var d   = die_symbol();
-    var rhs = number() || (valid=false); //lol //don't allow this to be empty
-    for(var i = 0; i < lhs; i++){
-      var a_roll = Math.floor(Math.random()*rhs)+1;
-      result += a_roll + " ";
-      total += a_roll;
-    }
-    result += ": " + total;
-  }
-  function die_symbol(){
-    var die_symbols = ['!','d'];
-    if(die_symbols.includes(peek())){
-      return pop();
-    } else {
-      valid = false;
-    }
-  }
-  function number(){
-    var digits = "";
-    while(peek()&&peek().match(/\d/)){
-      digits += pop();
-    }
-    return digits; //this will implicitly convert from string to number later
-  }
-  /* //might need to keep this test code to avoid annoying everyone with live debugging
-  var match = string.match(/([0-9]+)[d!]([0-9]+)/);
-  if(match){
-    
-  } else {
-    valid = false;
-  }
-  */
-  /*
-  if(!subresult.valid){
-    valid=false;
-    result += "[parse error]";
-  } else {
-    result += subresult.result;
-  }
-  */
-  expression();
-  return {result: result, valid: valid}; 
 }
 
 // THIS MUST BE THIS WAY
