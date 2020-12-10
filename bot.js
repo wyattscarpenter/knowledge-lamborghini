aa34d3f58cb1796ebe9d07f7b4462bfa792a6ef9
wyattscarpenter <wyattscarpenter@gmail.com>
2020-12-10 23:03:30 +0000

    try to also send the file

--- bot.js
+++ bot.js
@@ -40,7 +40,11 @@ client.on('message', message => {
      (resp) => {
        let data = '';
        resp.on('data', (chunk) => {data += chunk;});
-       resp.on('end', () => {channel.send("https://commons.wikimedia.org/wiki/"+JSON.parse(data.match(/"File[\s\S]*"/)[0]).replace(/\s/g,"_"));});
+       resp.on('end', () => {
+         channel.send("https://commons.wikimedia.org/wiki/"+JSON.parse(data.match(/"File[\s\S]*"/)[0]).replace(/\s/g,"_"),
+                      {files: ["https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/"+JSON.parse(data.match(/"File[\s\S]*"/)[0]).replace(/\s/g,"_")}
+         );
+       });
      }
     ).on("error", (err) => {console.log(err);});
     req.end();
