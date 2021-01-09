4d2756bc8f1c3cef0da7e92f5bb93bbbe1f31c60
wyattscarpenter <wyattscarpenter@gmail.com>
2021-01-09 11:36:46 +0000

    fix

--- bot.js
+++ bot.js
@@ -52,10 +52,9 @@ client.on('message', message => {
   }
   if(pokemon_answers[channel]){
     var target = pokemon_answers[channel].toLowerCase();
-    var target_sans_parenthetical_phrase = target.replace(/[^\(]*\)/g, '')
-    target_sans_parenthetical_phrase = target.replace(/\(/g, '')
-    if (target_sans_parenthetical_phrase){ //if the thing is empty, ignore the previous step
-      target = target_sans_parenthetical_phrase;
+    var target_sans_parenthetical_phrases = target.replace(/[^\(]*\)/g, '').target.replace(/\(/g, '');
+    if (target_sans_parenthetical_phrases){ //if the thing is empty, ignore the previous step
+      target = target_sans_parenthetical_phrases;
     }
     target = target.replace(/[^a-z]/g, '');
     var guess = message.content.toLowerCase().replace(/[^a-z]/g, '');
