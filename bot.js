58e671b143afec1ad6da7e041f244a4cd2cef7b7
wyattscarpenter <wyattscarpenter@gmail.com>
2023-05-16 01:46:41 -0700

    add per-server response feature and also refactor & retool some stuff slightly

--- bot.js
+++ bot.js
@@ -8,10 +8,17 @@ const client = new Discord.Client();
 
 const BOT_TOKEN = require("./token.json"); //this file is probably missing from your code base, initially, since I have it gitignored, as it is the secret bot token. Never fear! Go to discord and get a bot token of your own, and then put it in a new file called token.json in this directory, surrounding it in quotes to make a javascript string, "like this". That's all!
 var text = require('./text.json'); //At this time, this is the W. D. Ross 1908 translation of Nicomachean Ethics, as best I can tell.
+
 try{
   var responses = require('./responses.json'); //This provides persistent storage of responses.
 } catch {
   var responses = {};
+}
+
+try{
+  var server_responses = require('./server_responses.json'); //This provides persistent storage of responses.
+} catch {
+  var server_responses = {};
 }
 var texts = {};
 var channel;
@@ -27,9 +34,9 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.author.bot){return;} //don't let the bot respond to its own messages
   if (!message.content){return;} //don't even consider empty messages
-  
+
   channel = message.channel;
-  var m = message.content.toLowerCase();
+  const m = message.content.toLowerCase();
 
   if(message.mentions.has(client.user, {ignoreRoles: true, ignoreRepliedUser: true, ignoreEveryone: true})){
     if(m.includes("help")){
@@ -39,11 +46,11 @@ client.on('message', message => {
   //oldify reddit links.
   if ( m.match( /[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*\.?reddit\.com\/[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*/gmi) ){
   //here we check if null first to avoid crash on trying to iterate over null. having done that, we actually do the thing:
-    for (r of m.match( /[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*\.?reddit\.com\/[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*/gmi ) ){
+    for (let r of m.match( /[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*\.?reddit\.com\/[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*/gmi ) ){
       //huge character list from https://stackoverflow.com/a/1547940
       //note that it was easier to detect reddit urls and then futz with them each individually than do some crazy capture-jutsu. 
       if (!r.match( /[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*old\.?reddit\.com\/[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*/gmi)){ //It's already old reddit, do nothing.
-        target = r.match( /[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*\.?reddit\.com\/([\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*)/ )[1]; // "target" is not here meant to have any precise technical meaning, it's just all the url stuff after the first / (almost, but not quite, a "path").
+        let target = r.match( /[\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*\.?reddit\.com\/([\w\-.~:\/?#\[\]@!$&'\(\)\*+,;%=]*)/ )[1]; // "target" is not here meant to have any precise technical meaning, it's just all the url stuff after the first / (almost, but not quite, a "path").
         channel.send("<https://old.reddit.com/"+target+">"); //note that urls followed by eg a comma might include the comma, since it's technically a valid url to have that character at the end. This is arguably undesireable behavior.
       }
     }
@@ -57,12 +64,12 @@ client.on('message', message => {
       think();
     }else{
       channel.send("Demand me nothing. What you know, you know.");
-    }  
+    }
   }
   if (m === 'stop!') {
     stop_think();
   }
-  
+
   if (m === 'crash') {
     console.log("triggering crash in this program");
     channel.send("I'm crashing ova heya! [crashes]").then(crash);
@@ -72,10 +79,11 @@ client.on('message', message => {
   if(m.includes('nice dice')){
     message.channel.send("Sponsored by NiceDice™");
   }
-  if((result = nicedice.roll(m)).valid){
+  let nicedice_roll_result = nicedice.roll(m);
+  if(nicedice_roll_result.valid){
     console.log("rollin: "+JSON.stringify(m));
-    console.log("rollout: "+JSON.stringify(result));
-    message.channel.send(result.value+"\n`"+result.roll_record+"`");
+    console.log("rollout: "+JSON.stringify(nicedice_roll_result));
+    message.channel.send(nicedice_roll_result.value+"\n`"+nicedice_roll_result.roll_record+"`");
   }
 
   //extremely dumb features
@@ -87,14 +95,14 @@ client.on('message', message => {
     return distance(l,r) < l.length*.75;
   }
   if(pokemon_answers[channel]){
-    var target = pokemon_answers[channel].toLowerCase();
+    let target = pokemon_answers[channel].toLowerCase();
     target = target.replace(/[^\(]*\)/g, '').replace(/\(/g, '') || target;
     target = target.replace(/[^a-z]/g, '') || target;
-    var guess = m;
+    let guess = m;
     guess = guess.replace(/[^a-z]/g, '') || guess;
     //fuzzy string match
-    var normalized_distance = distance(target, guess) / target.length;
-    var distance_threshold = .75; // this tuning seems alright, in terms of false negative to false positive ratio, but the whole experience is still very difficult, which is regrettable
+    let normalized_distance = distance(target, guess) / target.length;
+    let distance_threshold = .75; // this tuning seems alright, in terms of false negative to false positive ratio, but the whole experience is still very difficult, which is regrettable
     if (normalized_distance < distance_threshold){
       channel.send("It's "+pokemon_answers[channel]+"!\nTarget: `"+target+"` Your Guess: `"+guess+"`.\nNormalized Distance (lower is better): "+normalized_distance+" Threshold: "+distance_threshold);
       delete pokemon_answers[channel];
@@ -103,58 +111,26 @@ client.on('message', message => {
   if (m === 'enumerate responses') {
     console.log(responses[channel]);
     send_long( channel, JSON.stringify(responses[channel]) );
+    console.log(server_responses[message.guild]);
+    send_long( channel, JSON.stringify(server_responses[message.guild]) );
   }
   if (m.startsWith("set ")) {
-    text_portion = m.split(/\s(.+)/)[1];
-    if(text_portion){ //guard against setting the empty set. not sure if this is needed.
-      thingum = text_portion.split(/\s(.+)/);
-      responses[channel] ??= {} //Gotta populate this entry, if need be, with an empty object to avoid an error in assigning to it in the next line.
-      responses[channel][thingum[0]] = thingum[1];
-      fs.writeFile("responses.json", JSON.stringify(responses), console.log);
-    }
+    the_function_that_does_setting_for_responses(message);
   }
-  if (m.startsWith("set-probabilistic ")) { //TODO: this may be getting stuck on the last option now, for some reason?! It worked before...
-    command_arguments_text = m.split(/\s(.+)/)[1]; // eg: s-p, (blah, (blah , blah blah blah))
-    number = command_arguments_text.split(/\s(.+)/)[0];
-    text_portion = command_arguments_text.split(/\s(.+)/)[1];
-    keyword = text_portion.split(/\s(.+)/)[0];
-    response = text_portion.split(/\s(.+)/)[1];
-    if(isNaN(number)){ //optional, default to 1 if there's nothing there.
-      //shift everything to the right (consider the eg diagram above for a sketch of what this is working on)
-      response = text_portion;
-      keyword = number;
-      number = 1;
-    }
-    responses[channel] ??= {} //Gotta populate this entry, if need be, with an empty object to avoid an error in assigning to it later
-    //Should we replace wholly an existing non-probabilistic response, or make it part of the new possibility range? Here, I've opted for the latter.
-    var current_guy = responses[channel][keyword];
-    if(is_string(current_guy)){
-      responses[channel][keyword] = {current_guy: 1}; //the extra square brackets are because it's a computed property: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Missing_colon_after_property_id#computed_properties
-    }
-    responses[channel][keyword] ??= {}
-    responses[channel][keyword][response] = number ; //note that there isn't presently any way to unset responses. They can be set to 0, however, or perhaps the whole object could be `set` to the empty string. The latter approach also removes it from enumerate responses, which is cool.
-
-    fs.writeFile("responses.json", JSON.stringify(responses), console.log);
+  if (m.startsWith("set-probabilistic ")) {
+    the_function_that_does_setting_for_responses(message, probabilistic=true);
+  }
+  if (m.startsWith("set-for-server ")) {
+    the_function_that_does_setting_for_responses(message, for_server=true);
+  }
+  if (m.startsWith("set-probabilistic-for-server ")) {
+    the_function_that_does_setting_for_responses(message, probabilistic=true, for_server=true);
   }
   if (responses[channel] && m in responses[channel]) { //guard against empty responses set for this channel
-    const r = responses[channel][m]; //the response might be a string or an object mapping from strings to weights.
-    if(is_string(r)){
-      r && channel.send(r); //guard against sending an empty string (which is a crashing error for us... maybe fix that with a wrapping function later?)
-    } else {
-      //pick by weighted randomness
-      //implicitly, the type is object mapping from string → int, with each int being the number of "tickets" the string has in the "raffle", so to speak.
-      var cumulative_weights = [];
-      for(const key of Object.keys(r)){
-        cumulative_weights.push(r[key]||1 + cumulative_weights.at(-1)||0);
-      }
-      const random = Math.random() * cumulative_weights.at(-1);
-      for(const key of Object.keys(r)){
-        if (random - cumulative_weights.shift() <= 0) {
-          key && channel.send(key);
-          break;
-        }
-      }
-    }
+    the_function_that_does_sending_for_responses(message);
+  }
+  if (server_responses[message.guild] && m in server_responses[message.guild]) { //guard against empty responses set for this channel
+    the_function_that_does_sending_for_responses(message, for_server=true);
   }
   if (m in global_responses) {
     channel.send(global_responses[m]);
@@ -164,22 +140,22 @@ client.on('message', message => {
 //implementation functions
 
 function update_status_clock(){ //This date is extremely precisely formatted for maximum readability in Discord's tiny area, and also familiarity and explicitness to users.
-  date = new Date(); //Want to avoid edge cases so we use the same Date object in each format call.
+  let date = new Date(); //Want to avoid edge cases so we use the same Date object in each format call.
   if(date.getSeconds()){return false;} //only update with minute resolution, on 00 seconds of each minute (Discord can't handle us using second resolution)
-  client.user.setActivity( new Intl.DateTimeFormat('en-US', {timeStyle: 'short', timeZone: 'America/Los_Angeles'}).format(date) + " PT " //time with hard-coded zone. Looks like 4:20 PM PT 
-                         + new Intl.DateTimeFormat('en-US', {weekday: 'short', timeZone: 'America/Los_Angeles'}).format(date) + " " //day of week (short name). Looks like Mon 
+  client.user.setActivity( new Intl.DateTimeFormat('en-US', {timeStyle: 'short', timeZone: 'America/Los_Angeles'}).format(date) + " PT " //time with hard-coded zone. Looks like 4:20 PM PT
+                         + new Intl.DateTimeFormat('en-US', {weekday: 'short', timeZone: 'America/Los_Angeles'}).format(date) + " " //day of week (short name). Looks like Mon
                          + new Intl.DateTimeFormat('en-GB', {dateStyle: 'medium', timeZone: 'America/Los_Angeles'}).format(date) //date, with day first. Looks like 9 Aug, 2021
                          );
   return true; //nota bene: the return value indicates if the function decided to update, but I don't use the return value anywhere else in the program so far.
 }
 
 function whos_that_pokemon(){
-  var req = https.request('https://commons.wikimedia.org/w/api.php?action=query&generator=random&grnnamespace=6&format=json',//image
+  let req = https.request('https://commons.wikimedia.org/w/api.php?action=query&generator=random&grnnamespace=6&format=json',//image
     (resp) => {
       let data = '';
       resp.on('data', (chunk) => {data += chunk;});
       resp.on('end', () => {
-        id = JSON.parse(data.match(/"File[\s\S]*"/)[0]);
+        let id = JSON.parse(data.match(/"File[\s\S]*"/)[0]);
         channel.send({files:[{
           attachment: "https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/"+encodeURIComponent(id),
           name: 'pokemon'+id.match(/\.\w*?$/)[0].toLowerCase()
@@ -194,10 +170,10 @@ function whos_that_pokemon(){
 
 function begin_think(){
   texts[channel] = text;
-}  
-  
+}
+
 function think(){
-  var s = texts[channel].shift();
+  let s = texts[channel].shift();
   if(s){
     send_long(channel, s);
     if(!intervals[channel]){intervals[channel] = setInterval(think, 1000*60*60*24);} //do it again in a day
@@ -213,7 +189,7 @@ function stop_think(){
 
 //Could overwrite the original send() 🤔. On the other hand, I could probably just upgrade discord.js to the newest version one of these days, maybe it's fixed there.
 function send_long(channel, string){ //.send() Just Fails for messages over discord's 2000 character limit
-  for(var l of string.split("\n")){ //break lines into separate messages
+  for(let l of string.split("\n")){ //break lines into separate messages
     while(l){
       channel.send(l.slice(0,2000)); //note that this is 0-indexed and excludes the specified end character
       l = l.slice(2000); //going over is fine, you know how it is
@@ -227,6 +203,68 @@ function crash(){
 
 function is_string(variable){
   return (typeof variable === 'string' || variable instanceof String)
+}
+
+function the_function_that_does_setting_for_responses(message, probabilistic=false, for_server=false){
+  const response_container = for_server? server_responses : responses;
+  const response_container_indexer = for_server? message.guild : message.channel;
+  const saving_file_name = for_server? "server_responses.json" : "responses.json";
+
+  if(probabilistic){
+    let command_arguments_text = message.content.split(/\s(.+)/)[1]; // structural diagram: set-probabilistic (blah, (blah , blah blah blah))
+    let number = command_arguments_text.split(/\s(.+)/)[0];
+    let text_portion = command_arguments_text.split(/\s(.+)/)[1];
+    let keyword = text_portion.split(/\s(.+)/)[0];
+    let response = text_portion.split(/\s(.+)/)[1];
+    if(isNaN(number)){ //optional, default to 1 if there's nothing there.
+      //shift everything to the right (consider the eg diagram above for a sketch of what this is working on)
+      response = text_portion;
+      keyword = number;
+      number = 1;
+    }
+    keyword = keyword.toLowerCase(); //lowercase the keyword
+    response_container[response_container_indexer] ??= {} //Gotta populate this entry, if need be, with an empty object to avoid an error in assigning to it later
+    //Should we replace wholly an existing non-probabilistic response, or make it part of the new possibility range? Here, I've opted for the latter.
+    let current_guy = response_container[response_container_indexer][keyword];
+    if(is_string(current_guy)){
+      response_container[response_container_indexer][keyword] = {current_guy: 1}; //the extra square brackets are because it's a computed property: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Missing_colon_after_property_id#computed_properties
+    }
+    response_container[response_container_indexer][keyword] ??= {}
+    response_container[response_container_indexer][keyword][response] = number ; //note that there isn't presently any way to unset responses. They can be set to 0, however, or perhaps the whole object could be `set` to the empty string. The latter approach also removes it from enumerate responses, which is cool.
+  } else {
+    const text_portion = message.content.split(/\s(.+)/)[1];
+    if(text_portion){ //guard against setting the empty set. not sure if this is needed.
+      let thingum = text_portion.split(/\s(.+)/);
+      response_container[response_container_indexer] ??= {} //Gotta populate this entry, if need be, with an empty object to avoid an error in assigning to it in the next line.
+      response_container[response_container_indexer][thingum[0].toLowerCase()] = thingum[1];
+    }
+  }
+  fs.writeFile(saving_file_name, JSON.stringify(response_container), console.log);
+}
+
+function the_function_that_does_sending_for_responses(message, for_server=false){
+  const response_container = for_server? server_responses : responses;
+  const response_container_indexer = for_server? message.guild : message.channel;
+  
+  //Should this be lowerCased?
+  const r = response_container[response_container_indexer][message.content.toLowerCase()]; //the response might be a string or an object mapping from strings to weights.
+  if(is_string(r)){
+    r && message.channel.send(r); //guard against sending an empty string (which is a crashing error for us... maybe fix that with a wrapping function later?)
+  } else {
+    //pick by weighted randomness
+    //implicitly, the type is object mapping from string → int, with each int being the number of "tickets" the string has in the "raffle", so to speak.
+    let cumulative_weights = [];
+    for(const key of Object.keys(r)){
+      cumulative_weights.push(r[key]||1 + cumulative_weights.at(-1)||0);
+    }
+    const random = Math.random() * cumulative_weights.at(-1);
+    for(const key of Object.keys(r)){
+      if (random - cumulative_weights.shift() <= 0) {
+        key && message.channel.send(key);
+        break;
+      }
+    }
+  }
 }
 
 // THIS MUST BE THIS WAY
--- readme.md
+++ readme.md
@@ -22,13 +22,17 @@ This is Knowledge Lamborghini (“kl”), an eclectic discord bot with exactly t
 * NO versioning, uptime, or stability guarantees. I keep the version number at 0.1.0 and commit broken features directly to master as I work on them. I may or may not have the bot running at any given time.
 * NO automatic testing paradigm, because I didn't think about how easy it would be to do that until I had built all the features.
 
-### Commands
+### Positive features
+
+A note about the commands: all commands are usually just as they are written here. Most commands have to be in their own message, and can't be put into the middle, beginning, or end, of another message. Parameters to commands (word or phrase used as data by the command) are noted below with descriptive phrases in square brackets, [like so]; replace the entire bracketed word/phrase, including the brackets, with the desired parameter, when invoking the command. Optional parameters are in parentheses. The order of parameters is significant, as that's what allows kl is figure out which parameter is intended to mean what.
+
 * @ the bot with a message containing "help" to get a link to this help document.
 * Type `think!` and kl begins to recite the book located in text.json, one unit per hour in the channel in which you made the command. `stop!` ceases the madness.
 * Type a phrase that's something like `who's that pokemon?` and kl posts an image from wikimedia for you to guess the file name of. The first successful guess for any picture is acknowledged.
 * Using the power of NiceDice (https://github.com/wyattscarpenter/nicedice), kl interprets any message consisting solely of a dnd-style dice expression, generates random numbers to simulate dice rolls, and outputs the result.
-* Type `set [word] [response message]` and kl will respond with the response message when the word is posted in the channel. This is a per-channel setting! You can remove a setting by setting it to the empty string.
-* `set-probabilistic ([number]) [word] [response message]` is the same as `set`, but sets a response in a pool of responses to be randomly selected, weighted by the number given (metaphorically, you can think of the response as a raffle, in which the response has [number] tickets')—if no number is given, that quantity defaults to 1 (if you'd like set [word] to be a number, you must provide [number], to avoid ambiguity). Naturally, to give any randomness to the effect, this command must be used multiple times. Also, using set-probabilistic on a word that already has a response set by `set` will turn that response into a one-ticket possibility.
+* Type `set [word] [response message]` and kl will respond with the response message when the word is posted in the channel, by itself in a message (case-insensitive). This is a per-channel setting! You can remove a setting by setting it to the empty string.
+* `set-probabilistic (number) [word] [response message]` is the same as `set`, but sets a response in a pool of responses to be randomly selected, weighted by the number given (metaphorically, you can think of the response as a raffle, in which the response has [number] tickets')—if no number is given, that quantity defaults to 1 (if you'd like set [word] to be a number, you must provide [number], to avoid ambiguity). Naturally, to give any randomness to the effect, this command must be used multiple times. Also, using set-probabilistic on a word that already has a response set by `set` will turn that response into a one-ticket possibility.
+* `set-for-server` and `set-probabilistic-for-server` are the server-wide equivalents of the above commands. The banks of server responses and channel responses are different, so if you make overlapping responses kl might double-reply, etc.
 * `enumerate responses` lists all the responses for the channel.
 * Type `crash` and kl will crash. Please do not do this.
 * Reddit link “oldification”: links to reddit.com pages are immediately reposted as links to old.reddit.com pages.
