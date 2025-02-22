bf74c629a9a6f64e613858d1473d85768457de68
wyattscarpenter <wyattscarpenter@gmail.com>
2025-02-22 06:27:46 -0800

    Update bot.js: I guess id is a string, possibly for int precision reasons in js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const client = new Client({
   partials: [Partials.Message, Partials.Reaction],
 });
 
-const kl_test_emoji_id = 1342850037792772106 //this is a test emoji I created when we need to test an emoji feature.
+const kl_test_emoji_id = "1342850037792772106" //this is a test emoji I created when we need to test an emoji feature.
 
 // THIS LINE MUST HAPPEN FOR THE BOT TO LOGIN:
 client.login(require("./token.json"));//this file is probably missing from your code base, initially, since I have it gitignored, as it is the secret bot token. Never fear! Go to discord and get a bot token of your own, and then put it in a new file called token.json in this directory, surrounding the token in quotes to make a javascript string, "like this". That's all!
