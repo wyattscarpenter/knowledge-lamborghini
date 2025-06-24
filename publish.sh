9b696133b4615011a7bf2b3ee38752ac069a19d0
wyattscarpenter <wyattscarpenter@gmail.com>
2025-06-24 04:50:35 -0700

    order publish.sh steps so I can fail earlier

--- publish.sh
+++ publish.sh
@@ -3,7 +3,6 @@ if [[ "$version" != v* ]]; then
   version="v$version"
 fi
 read -p "Publishing as version $version (first argument to the publish command) once you press enter..."
-git tag "$version"
-git push
-git push --tag
-npm publish
+# These are set up in approximate order of when you might want to be alerted that something is wrong.
+#   Although it's hard to say that order holds exactly.
+git push && git tag "$version" && git push --tag && npm publish
