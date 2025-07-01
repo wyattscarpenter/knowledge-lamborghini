45beeb56aecb61a11717b972946fb18bd279268a
wyattscarpenter <wyattscarpenter@gmail.com>
2025-07-01 06:24:37 -0700

    figure out how to get the npm version programmatically

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-version=`npm pkg get version --workspaces=false`
+version=v`npm pkg get version --workspaces=false | tr -d '"'`
 read -p "Publishing as version $version (first argument to the publish command) once you press enter..."
 # These are set up in approximate order of when you might want to be alerted that something is wrong.
 #   Although it's hard to say that order holds exactly all the time.
