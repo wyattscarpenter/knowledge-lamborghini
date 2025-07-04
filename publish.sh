3f2a278a8759f9a3e007957ae2ded1f7bd7b4a8a
wyattscarpenter <wyattscarpenter@gmail.com>
2025-07-04 08:12:09 -0700

    remove now-wrong docu from ./publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 version=v`npm pkg get version --workspaces=false | tr -d '"'`
-read -p "Publishing as version $version (first argument to the publish command) once you press enter..."
+read -p "Publishing as version $version once you press enter..."
 # These are set up in approximate order of when you might want to be alerted that something is wrong.
 #   Although it's hard to say that order holds exactly all the time.
 git push && git tag "$version" && git push --tag && npm publish
